db06f35d3ef7cb8c9c48df0e6e11bbb04b53949b
Jaroslav Merxbauer <merxbj@hotmail.com>
2009-10-26 00:01:43 +0100

    File Checker passed PQM3 File checker now allows to register/unregister itself as a windows service by command line parameters /i and /u.

--- cpp/VS2005/FileChecker/FileCheckerSvcs.cpp
+++ cpp/VS2005/FileChecker/FileCheckerSvcs.cpp
@@ -1,17 +1,34 @@
 #include <windows.h>
 #include <stdio.h>
+#include "resource.h"
 #include "FileCheckerSvcs.h"
 
-void main(int argc, char** argv) 
-{ 
-    switch (ParseCallerParameters(argc, argv))
+int _tmain(int argc, LPTSTR* argv)
+{
+	g_errorCode = ERROR_NONE;
+
+	switch (ParseCallerParameters(argc, argv))
 	{
-	case REGISTER_SERVICE:
-		RegisterService(L"FileChecker", L"File Checker", L"");
+	case REQUEST_REGISTER:
+		g_errorCode = RegisterService(L"FileChecker", L"File Checker");
 		break;
+
+	case REQUEST_UNREGISTER:
+		g_errorCode = UnregisterService(L"FileChecker");
+		break;
+
+	case REQUEST_RUN:
+		RunService();
+		break;
+
+	default:
+		g_errorCode = ERROR_INVALID_REQUEST;
+	}
+
+	return HandleErrorCode(g_errorCode);
 }
 
-void StartService()
+void RunService()
 {
 	SERVICE_TABLE_ENTRY ServiceTable[2];
     ServiceTable[0].lpServiceName = L"FileChecker";
@@ -21,16 +38,14 @@ void StartService()
     ServiceTable[1].lpServiceProc = NULL;
     
 	// Start the control dispatcher thread for our service
-    StartServiceCtrlDispatcher(ServiceTable);
+	StartServiceCtrlDispatcher(ServiceTable);
 }
 
 
 void ServiceMain(int argc, char** argv)
 {
-    int error; 
- 
-    ServiceStatus.dwServiceType        = SERVICE_WIN32; 
-    ServiceStatus.dwCurrentState       = SERVICE_START_PENDING; 
+	ServiceStatus.dwServiceType        = SERVICE_WIN32;
+    ServiceStatus.dwCurrentState       = SERVICE_START_PENDING;
     ServiceStatus.dwControlsAccepted   = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
     ServiceStatus.dwWin32ExitCode      = NO_ERROR; 
     ServiceStatus.dwServiceSpecificExitCode = NO_ERROR; 
@@ -43,16 +58,17 @@ void ServiceMain(int argc, char** argv)
     if (hStatus == (SERVICE_STATUS_HANDLE)0) 
     { 
         // Registering Control Handler failed
-        return; 
+        g_errorCode = ERROR_REGISTER_SERVICE_CTRL_HANDLER;
+		return;
     }  
     // Initialize Service 
-    error = InitService(); 
-    if (error) 
+    if (!InitService()) 
     {
 		// Initialization failed
         ServiceStatus.dwCurrentState       = SERVICE_STOPPED; 
         ServiceStatus.dwWin32ExitCode      = -1; 
         SetServiceStatus(hStatus, &ServiceStatus); 
+		g_errorCode = ERROR_INIT_SERVICE;
         return; 
     } 
     // We report the running status to SCM. 
@@ -67,7 +83,7 @@ void ServiceMain(int argc, char** argv)
 // Service initialization
 bool InitService() 
 {
-    return true;
+	return true;
 }
 
 // Control handler function
@@ -99,28 +115,34 @@ void ControlHandler(DWORD request)
     return; 
 } 
 
-bool RegisterService(const TCHAR* serviceName, const TCHAR* serviceDisplayName, const TCHAR* serviceBinPath)
+int RegisterService(const TCHAR* serviceName, const TCHAR* serviceDisplayName)
 {
 	if ((_tcslen(serviceName) == 0) ||
-		(_tcslen(serviceDisplayName) == 0) ||
-		(_tcslen(serviceBinPath) == 0))
+		(_tcslen(serviceDisplayName) == 0))
 	{
-		return false;
+		return ERROR_INVALID_ARGS;
 	}
 
-	SC_HANDLE scmH = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+	TCHAR szPath[MAX_PATH+1] = TEXT("");
+	if (!GetModuleFileName( NULL, szPath, MAX_PATH))
+	{
+        return ERROR_GET_MODULE_FILE_NAME;
+	}
+
+
+	SC_HANDLE scmH = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
 
 	if (scmH == NULL)
-		return false;
+		return ERROR_OPEN_SC_MANAGER;
 
 	SC_HANDLE scH = CreateService(scmH,
 		serviceName,
 		serviceDisplayName,
-		SC_MANAGER_CREATE_SERVICE,
-		SERVICE_WIN32,
-		SERVICE_AUTO_START,
-		SERVICE_ERROR_IGNORE,
-		serviceBinPath,
+		SERVICE_ALL_ACCESS,
+		SERVICE_WIN32_OWN_PROCESS,
+		SERVICE_DEMAND_START,
+		SERVICE_ERROR_NORMAL,
+		szPath,
 		NULL,
 		NULL,
 		NULL,
@@ -128,11 +150,115 @@ bool RegisterService(const TCHAR* serviceName, const TCHAR* serviceDisplayName, 
 		NULL);
 	
 	if (scH == NULL)
-		return false;
+		return ERROR_CREATE_SERVICE;
 
-	return true;
+	CloseServiceHandle(scmH);
+	CloseServiceHandle(scH);
+
+	return 0;
+}
+
+RequestType ParseCallerParameters(int argc, LPTSTR* argv)
+{
+	if (argc == 1)
+		return REQUEST_RUN;
+
+	if (argc > 2)
+		return REQUEST_INVALID;
+
+	if (argc == 2)
+	{
+		if (_tcscmp(argv[1], L"/i") == 0)
+			return REQUEST_REGISTER;
+		else if (_tcscmp(argv[1], L"/u") == 0)
+			return REQUEST_UNREGISTER;
+		else
+			return REQUEST_INVALID;
+	}
+
+	return REQUEST_INVALID;
+}
+
+int UnregisterService(TCHAR* serviceName)
+{
+	if (_tcslen(serviceName) == 0) 
+	{
+		return ERROR_INVALID_ARGS;
+	}
+
+	SC_HANDLE scmH = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+
+	if (scmH == NULL)
+		return ERROR_OPEN_SC_MANAGER;
+
+	SC_HANDLE scH = OpenService(scmH, serviceName, SERVICE_ALL_ACCESS);
+	
+	if (scH == NULL)
+		return ERROR_OPEN_SERVICE;
+
+	return (DeleteService(scH) == TRUE) ? ERROR_NONE : ERROR_DELETE_SERVICE;
+}
+
+int HandleErrorCode(int errorCode)
+{
+	switch (errorCode)
+	{
+	case ERROR_NONE:
+		break; // we will not log succeesses
+	case ERROR_INVALID_ARGS:
+		log(L"Programmer mistake - function got invalid arguments.\n");
+		break;
+	case ERROR_INVALID_REQUEST:
+		log(L"User mistake - program got invalid arguments.\n");
+		break;
+	case ERROR_REGISTER_SERVICE_CTRL_HANDLER:
+		log(L"System error - program couldn't start the service.\n");
+		break;
+	case ERROR_INIT_SERVICE:
+		log(L"Application error - couldn't initialize the service.\n");
+		break;
+	case ERROR_OPEN_SC_MANAGER:
+		log(L"System error - program couldn't open the service controll manager.\n");
+		break;
+	case ERROR_CREATE_SERVICE:
+		log(L"System error - program couldn't register a service.\n");
+		break;
+	case ERROR_OPEN_SERVICE:
+		log(L"System error - program couldn't open a service.\n");
+		break;
+	case ERROR_DELETE_SERVICE:
+		log(L"System error - program couldn't delete a service.\n");
+		break;
+	case ERROR_GET_MODULE_FILE_NAME:
+		log(L"System error - program couldn't retrieve a module file path.\n");
+		break;
+	default:
+		log(L"Fatal error - program encountered a serious unhandler errror.\n");
+	}
+	
+	return errorCode;
+}
+
+void log(TCHAR* message)
+{
+	if (message == NULL)
+		return;
+	
+	FILE* f = fopen(LOG_FILE_PATH, "a+");
+	if (f == NULL)
+		return;
+
+	_ftprintf(f, L"%s\n", message);
+
+	fclose(f);
 }
 
 void Run()
 {
+	while (ServiceStatus.dwCurrentState == SERVICE_RUNNING)
+	{
+		log(L"Up and running!");
+		/* Add file checking code here */
+		Sleep(5000);
+	}
 }
--- cpp/VS2005/FileChecker/FileCheckerSvcs.h
+++ cpp/VS2005/FileChecker/FileCheckerSvcs.h
@@ -3,14 +3,50 @@
 
 #include "TCHAR.H"
 
+#define WM_NOTIFY_ICON WM_USER+1;
+
+enum
+{
+	ERROR_NONE,
+	ERROR_INVALID_ARGS,
+	ERROR_INVALID_REQUEST,
+	ERROR_REGISTER_SERVICE_CTRL_HANDLER,
+	ERROR_INIT_SERVICE,
+	ERROR_OPEN_SC_MANAGER,
+	ERROR_CREATE_SERVICE,
+	ERROR_OPEN_SERVICE,
+	ERROR_DELETE_SERVICE,
+	ERROR_FATAL_ERROR,
+	ERROR_GET_MODULE_FILE_NAME
+};
+
+int g_errorCode = ERROR_NONE;
+
+const char LOG_FILE_PATH[] = "C:\\temp\\error.log";
+
 SERVICE_STATUS ServiceStatus; 
 SERVICE_STATUS_HANDLE hStatus; 
  
-void StartService();
+typedef enum RequestType_t
+{
+	REQUEST_INVALID = -1,
+	NO_REQUEST = 0,
+	REQUEST_REGISTER,
+	REQUEST_UNREGISTER,
+	REQUEST_RUN
+} RequestType;
+
+void RunService();
 void ServiceMain(int argc, char** argv); 
 void ControlHandler(DWORD request);
 bool InitService();
-bool RegisterService(TCHAR* serviceName, TCHAR* serviceDisplayName, TCHAR* serviceBinPath);
+int RegisterService(const TCHAR* serviceName, const TCHAR* serviceDisplayName);
+int UnregisterService(TCHAR* serviceName);
 void Run();
+RequestType ParseCallerParameters(int argc, LPTSTR* argv);
+
+int HandleErrorCode(int errorCode);
+
+void log(TCHAR* message);
 
 #endif
