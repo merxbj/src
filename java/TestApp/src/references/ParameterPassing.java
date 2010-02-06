ed9ffd6775c60a471049acde1df686884e8077b4
unknown <eTeR@.(none)>
2010-02-06 23:47:33 +0100

    Fixed last commit - missing files!

--- java/TestApp/src/references/ParameterPassing.java
+++ java/TestApp/src/references/ParameterPassing.java
@@ -9,18 +9,26 @@ package references;
  *
  * @author eTeR
  */
+
+import java.util.*;
+
 public class ParameterPassing {
+    
     public static void main(String[] args) {
 
-        SimpleClass sc = new SimpleClass(1);
+        ArrayList<SimpleClass> sc = new ArrayList<SimpleClass>();
 
-        System.out.println(String.format("%d", sc.member));
-        modify(sc);
-        System.out.println(String.format("%d", sc.member));
+        sc.add(new SimpleClass(7));
+        sc.add(new SimpleClass(2));
+        sc.add(new SimpleClass(5));
+        sc.add(new SimpleClass(10));
 
-    }
+        Collections.sort(sc, new SimpleClassComp());
 
-    private static void modify(SimpleClass o) {
-        o = new SimpleClass(2);
+        for (SimpleClass sc1 : sc) {
+            System.out.println(String.format("%d", sc1.member));
+        }
+
+        String[] str = new String[10];
     }
 }
--- java/TestApp/src/references/SimpleClass.java
+++ java/TestApp/src/references/SimpleClass.java
@@ -9,7 +9,12 @@ package references;
  *
  * @author eTeR
  */
-public class SimpleClass {
+public class SimpleClass implements Comparable<SimpleClass> {
+
+    public int compareTo(SimpleClass o) {
+        return ((Integer) o.member).compareTo(member);
+    }
+
 
     public SimpleClass(int member) {
         this.member = member;
