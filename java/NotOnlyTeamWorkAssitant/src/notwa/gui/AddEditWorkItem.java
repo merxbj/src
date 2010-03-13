dae301444e32b1005ba5f7f312af1d91498b269a
mrneo <mrneo@mrneo.(none)>
2010-03-13 03:38:04 +0100

    NOTWA - removing unnecesary files

--- java/NotOnlyTeamWorkAssitant/src/notwa/gui/AddEditWorkItem.java
+++ java/NotOnlyTeamWorkAssitant/src/notwa/gui/AddEditWorkItem.java
@@ -1,10 +1,10 @@
 package notwa.gui;
-
+ 
 import java.awt.BorderLayout;
-
+ 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+ 
 @SuppressWarnings("serial")
 public class AddEditWorkItem extends JFrame {
 	private String dialogType;
@@ -14,9 +14,9 @@ public class AddEditWorkItem extends JFrame {
 		this.setTitle("NOTWA - NOT Only Team Work Assistent - " + dialogType);
 		this.setSize(750,300);
 		this.setLocationRelativeTo(null);
-		
+		 
 		this.add(new JLabel("window for " + dialogType));
-		
+		 
 		this.setVisible(true);
 	}
 }
