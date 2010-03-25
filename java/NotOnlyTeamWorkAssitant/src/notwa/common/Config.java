88cae652b3e09a3cbc11ba9600f7acf5850e987f
merxbj <merxbj@hotmail.com>
2010-03-26 00:09:21 +0100

    NOTWA - Continuous improvement - to start using javadoc
    Javadoc, which is mandatory for the semestral project, is now being
    continuousl added to the project source files starting from the common
    package.
    Files covered:
      - Config.java
      - ConnectionInfo.java
    Moreover, very cosmetic refactoring has been made to have the source files
    following the Java Code Convetions properly.
    However, there is one exception from the Java Code Convetions - there is
    no strict line length requirement as I consider it outdated.

--- java/NotOnlyTeamWorkAssitant/src/notwa/common/Config.java
+++ java/NotOnlyTeamWorkAssitant/src/notwa/common/Config.java
@@ -1,26 +1,62 @@
+/*
+ * Config
+ *
+ * Conifg.java v0.0.0.5
+ *
+ * 2005-03-05
+ *
+ * Copyright (C) 2010  Jaroslav Merxbauer
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package notwa.common;
 
-import java.io.*;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.*;
-import javax.xml.xpath.*;
-import notwa.common.LoggingInterface;
+import java.io.File;
 import java.util.Collection;
 import java.util.TreeSet;
 import java.util.Set;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
 
+/**
+ * Config is the utility class for acquiring the data from configuration file.
+ * Config is written and suposed to be used as a singleton to provide sole
+ * instance operating over the single XML file stored in the hardcoded path in
+ * the system.
+ * <p>Even thought there is no motivation to have something written to the config
+ * file, so there is no actual motivation to have this class as singleton,
+ * it is discouraged to change this behavior as the need to persist any kind
+ * of information here could arise in future. This would then may cause the
+ * trouble.</p>
+ *
+ * @author  Jaroslav Merxbauer
+ * @version %I% %G%
+ */
 public class Config {
 
-    private static Config singleton;
+    private static Config instance;
+    private Set<ConnectionInfo> connections;
+    private File configFile;
     private final String CONFIG_FILE_NAME = "./notwa.config";
     
-    public static Config getInstance() {
-        if (singleton == null)
-            singleton = new Config();
-        return singleton;
-    }
-
+    /**
+     * Hidden constructor to prevent instationing the class from outside world.
+     */
     protected Config() {
         this.configFile = new File(CONFIG_FILE_NAME);
         this.connections = new TreeSet<ConnectionInfo>();
@@ -31,22 +67,54 @@ public class Config {
         }
     }
 
+    /**
+     * Gets the sole instance of the Config class which is maintained as the
+     * static singleton.
+     * 
+     * @return The singleton instance
+     */
+    public static Config getInstance() {
+        if (instance == null)
+            instance = new Config();
+        return instance;
+    }
+
+    /**
+     * Gets all connection information parsed from the configuration file
+     * Every conneection information is wrapped into the <code>ConnectionInfo</code>
+     * instance and all these instances are kept inside single <code>Collection</code>.
+     *
+     * @return <code>Collection</code> of all <code>ConnectionInfo</code>.
+     */
     public Collection<ConnectionInfo> getConnecionStrings() {
-        return  connections;
+        return connections;
 
     }
 
+    /**
+     * Parse the XML configuration document utilizing the DOM {@link Document}.
+     * 
+     * @throws Exception If the config file does not exist.
+     */
     public void parse() throws Exception {
         if (configFile.exists()) {
             DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             Document dom = db.parse(configFile);
-            parseConnecionStrings(dom.getElementsByTagName("Database"));
+            parseConnecionInformations(dom.getElementsByTagName("Database"));
         } else {
             throw new Exception("Config file does not exists!");
         }
     }
 
-    private void parseConnecionStrings(NodeList nodes) throws Exception {       
+    /**
+     * Parses out all connection informations from the provided <code>NodeList</code>
+     * utilizing the {@link XPath} and adding them into the <code>Collection</code>
+     * implemented as <code>Set</code>.
+     *
+     * @param odes XML nodes containing the actual connection information
+     * @throws Exception If an error occures during the XPath evaluation
+     */
+    private void parseConnecionInformations(NodeList nodes) throws Exception {
         XPath xp = XPathFactory.newInstance().newXPath();
         for (int i = 0; i < nodes.getLength(); i++) {
             Node n = nodes.item(i);
@@ -60,7 +128,4 @@ public class Config {
             connections.add(ci);
         }
     }
-
-    private Set<ConnectionInfo> connections;
-    private File configFile;
 }
--- java/NotOnlyTeamWorkAssitant/src/notwa/common/ConnectionInfo.java
+++ java/NotOnlyTeamWorkAssitant/src/notwa/common/ConnectionInfo.java
@@ -1,5 +1,45 @@
+/*
+ * ConnectionInfo
+ *
+ * ConnectionInfo.java v0.0.2.1
+ *
+ * 2005-03-05
+ *
+ * Copyright (C) 2010  Jaroslav Merxbauer
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package notwa.common;
 
+/**
+ * ConnectionInfo encapsulates all required connection information to be able
+ * to connect to the database with the proper credetials. These values are:
+ * <ul>
+ * <li>Host name</li>
+ * <li>Port number</li>
+ * <li>Database name</li>
+ * <li>User name</li>
+ * <li>Password</li>
+ * </ul>
+ * ConnectionInfo also encapsulates the label which might be used to distinguish
+ * between several connections easily.
+ *
+ * @author Jaroslav Merxbauer
+ * @version %I% %G%
+ */
 public class ConnectionInfo implements Comparable<ConnectionInfo> {
     private String label;
     private String host;
@@ -7,7 +47,21 @@ public class ConnectionInfo implements Comparable<ConnectionInfo> {
     private String dbname;
     private String user;
     private String password;
-    
+
+    /**
+     * Implemntation of the {@link Comparable} interface.
+     * Compares two <code>ConnectionInfo</code> subsequently by their:
+     * <ol>
+     * <li>Host name</li>
+     * <li>Port number</li>
+     * <li>Database name</li>
+     * <li>User name</li>
+     * </ol>
+     *
+     * @param ci The actual <code>ConnectionInfo</code> to be compared with.
+     * @return  The usual compareTo value resulting from the latest non-zero
+     *          comparsion based on the orded described above.
+     */
     @Override
     public int compareTo(ConnectionInfo ci) {
         int compare = host.compareTo(ci.host);
@@ -22,10 +76,19 @@ public class ConnectionInfo implements Comparable<ConnectionInfo> {
         }
         return compare;
     }
+
+    /**
+     * Compares the given <code>Object</code> with the actual instance.
+     * It utilizes the <code>compareTo</code> method the decrease the coding
+     * and maintenance effort.
+     *
+     * @param obj The actual <codee>Object</code> to be compared with.
+     * @return  <code>true</code> if both <code>Object</code> are the same in terms of values
+     *          <code>false</code> otherwise.
+     */
     @Override
-    
     public boolean equals(Object obj) {
-        if (!(obj instanceof ConnectionInfo)) { 
+        if ((obj == null) || !(obj instanceof ConnectionInfo)) {
             return false;
         } else {
             return this.compareTo((ConnectionInfo) obj) == 0 ;
@@ -41,42 +104,119 @@ public class ConnectionInfo implements Comparable<ConnectionInfo> {
         return hash;
     }
     
+    /**
+     * Sets the label of this <code>ConnectionInfo</code> instance.
+     * This label could be used to easily distguish between several different
+     * instances.
+     * 
+     * @param label The actual label which should be as much descriptive as
+     *              possible to easily identify this <code>ConnectionInfo</code>.
+     */
     public void setLabel(String label) {
         this.label = label;
     }
     
+    /**
+     * Sets the host name where the desired database server is running.
+     * The host name could be either
+     * <ul>
+     * <li>IP adress or</li>
+     * <li>URL which could be translated into the actual IP adress</li>
+     * </ul>
+     * 
+     * @param host The IP adress or URL of the database server.
+     */
     public void setHost(String host) {
         this.host = host;
     }
     
+    /**
+     * Sets the port number where the desired database server is listining.
+     * 
+     * @param port
+     */
     public void setPort(String port) {
         this.port = port;
     }
     
+    /**
+     * Sets the database name where the data for this application is stored.
+     * <p>Please make sure that the database you are connecting to matches the
+     * schema which is expected by the actual version of the NOTWA application.</p>
+     * 
+     * @param dbname The actual database name.
+     */
     public void setDbname(String dbname) {
         this.dbname = dbname;
     }
     
+    /**
+     * Sets the user name which should have the permission to access the database.
+     * <p>Please make sure that the requested user has all permission available
+     * to work with the choosen database</p>
+     * 
+     * @param user The actual user name.
+     */
     public void setUser(String user) {
         this.user = user;
     }
     
+    /**
+     * Sets the password, which is not encrypted, yet, assigned to the choosen
+     * user.
+     * 
+     * @param password The actual, non-encrypted, password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
     
+    /**
+     * Gets the label of this <code>ConnectionInfo</code> instance.
+     * This label could be used to easily distguish between several different
+     * instances.
+     * 
+     * @return  The actual label which should be as much descriptive as
+     *          possible to easily identify this <code>ConnectionInfo</code>.
+     */
     public String getLabel() {
         return this.label;
     }
 
+    /**
+     * Gets the password, which is not encrypted, yet, assigned to the choosen
+     * user.
+     * 
+     * @return The actual, non-encrypted, password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Gets the user name which should have the permission to access the database.
+     * <p>Please make sure that the requested user has all permission available
+     * to work with the choosen database</p>
+     * 
+     * @return The actual user name.
+     */
     public String getUser() {
         return user;
     }
 
+    /**
+     * Build the connection string which is accepted by the {@link jdbc} to
+     * connect to the requested database.
+     * <p>Currently only the mysql is supported and therefore assumed to be used
+     * as a part of the resulted connection string.</p>
+     * <p>The connections string also specifies the method how to work with zero
+     * <code>DateTime</code> values stored in the database. Without this
+     * specification, the <code>getTimestamp</code> method would throw an <code>Exception</code>
+     * which would make the <code>ResultSet</code> processing obscure.
+     *
+     * @return The actual connection string in format:
+     *         <p><code>jdbc:mysql://host:port/dbname?options</code></p>
+     */
     public String compileConnectionString() {
         String cs = String.format("jdbc:mysql://%s:%s/%s?zeroDateTimeBehavior=convertToNull",
                 this.host,
