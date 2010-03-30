f09a1ce62ccdbf3a80bb2ba70c687f4ac3a31846
merxbj <merxbj@hotmail.com>
2010-03-30 08:59:36 +0200

    NOTWA - Removing unused classes
    I have missed them in my last commit.

--- java/NotOnlyTeamWorkAssitant/src/notwa/dal/Fillable.java
+++
@@ -1,9 +0,0 @@
-package notwa.dal;
-
-import notwa.sql.ParameterSet;
-import notwa.wom.BusinessObjectCollection;
-
-public interface Fillable<T extends BusinessObjectCollection> {
-    int Fill(T boc);
-    int Fill(T boc, ParameterSet pc);
-}
--- java/NotOnlyTeamWorkAssitant/src/notwa/dal/Getable.java
+++
@@ -1,9 +0,0 @@
-package notwa.dal;
-
-import notwa.sql.ParameterSet;
-import notwa.wom.BusinessObject;
-import notwa.exception.DalException;
-
-public interface Getable<T extends BusinessObject> {
-        T get(ParameterSet primaryKey) throws DalException;
-}
