3f41f06c6a1d006c657fdb63600a100b51c11a88
merxbj <merxbj@hotmail.com>
2010-04-23 17:56:32 +0200

    TestApp - Another brute force solver
    This solver actually doesn't work for comple sudokus. This is just a proof
    of this.

--- java/TestApp/src/sudoku/SolverMain.java
+++ java/TestApp/src/sudoku/SolverMain.java
@@ -37,12 +37,9 @@ public class SolverMain {
 
         int [][] result;
 
-        for (SudokuSolver s : solvers) {
-            /*result = s.solve(matrix);
-            printMatrix(result);*/
-            result = s.solve(hardMatrix);
-            printMatrix(result);
-        }
+        SudokuSolver s = new LastRemainingSolver();
+        result = s.solve(hardMatrix);
+        printMatrix(result);
     }
 
     public static void printMatrix(int[][] matrix) {
