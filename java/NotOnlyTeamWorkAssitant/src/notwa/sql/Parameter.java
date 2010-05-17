17e8f1bffc390130456928cca4a42680eff09b0e
merxbj <merxbj@hotmail.com>
2010-05-17 19:54:47 +0200

    NOTWA - "Verified" should not be visible
    The previous fix didn't work because the SQL Parameter class traited to be
    equal to only by comparsion the parameter NAME. This is wrong, because
    you can define more parameters with the same name but with different value
    (e.g. status == VERIFIED AND status == CLOSED).
    I have therefore fixed the equals and hasCode methods to compare the
    Parameter by the name and the value.

--- java/NotOnlyTeamWorkAssitant/src/notwa/sql/Parameter.java
+++ java/NotOnlyTeamWorkAssitant/src/notwa/sql/Parameter.java
@@ -52,29 +52,31 @@ public class Parameter {
         this.value = value;
         this.relation = relation;
     }
-    
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
+
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
-        Parameter other = (Parameter) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
+        }
+        final Parameter other = (Parameter) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
             return false;
+        }
+        if (this.value != other.value && (this.value == null || !this.value.equals(other.value))) {
+            return false;
+        }
         return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 83 * hash + (this.value != null ? this.value.hashCode() : 0);
+        return hash;
     }
     
     /**
