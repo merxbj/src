dca43b95ed64c21c4111b950a77121b1d604c533
merxbj <merxbj@hotmail.com>
2010-09-13 13:11:24 +0200

    NOTWA - Whole project removed from merxbj/src repo.
    The sources could be found under merxbj/notwa repo.

--- java/NotOnlyTeamWorkAssitant/ddl/Create_Project.sql
+++
@@ -1,8 +0,0 @@
--- Create Projects table
-CREATE TABLE Project (
-    project_id              NUMERIC(19) NOT NULL,
-    name                    VARCHAR(255) NOT NULL,
-    
-    CONSTRAINT PK_Project
-        Primary Key (project_id)
-    )
