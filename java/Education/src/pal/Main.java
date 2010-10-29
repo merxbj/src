4f4092da68482c585b44a7c715560445734d93ae
merxbj <merxbj@hotmail.com>
2010-10-29 14:47:13 +0200

    Education - PAL - Graph skeleton

--- java/Education/src/pal/Main.java
+++ java/Education/src/pal/Main.java
@@ -1,7 +1,5 @@
-package pal;
-
 /*
- * Client
+ * Main
  *
  * Copyright (C) 2010  Jaroslav Merxbauer
  *
@@ -20,39 +18,94 @@ package pal;
  *
  */
 
-import java.net.*;
-import java.io.*;
+package pal;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  *
- * @author Jiri Fric
- * @version %I% %G%
+ * @author Jaroslav Merxbauer
  */
 public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
+        // TODO code application logic here
         try {
+            sit();
+        }
+        catch (Exception ex) {
+            System.out.print("-1");
+            System.out.flush();
+        }
+    }
 
-            StringBuilder builder = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            while (reader.ready()) {
-                builder.append(reader.readLine()).append("\n");
+    public static void sit() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());//pocet budov
+        int I = Integer.parseInt(reader.readLine());//pocet pripojeni
+        List<Hrana> pole = new ArrayList<Hrana>();
+        int odkud =-1;
+        int kam =-1;
+        int cena =-1;
+
+        while (odkud !=0 || kam!=0 || cena!=0){
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            odkud = Integer.parseInt(st.nextToken());
+            kam = Integer.parseInt(st.nextToken());
+            cena = Integer.parseInt(st.nextToken());
+            if (odkud!=0 || kam!=0 || cena!=0) {
+                if (odkud > kam){
+                    Hrana h = new Hrana(kam,odkud,cena);
+                    pole.add(h);
+                } else {
+                    Hrana h = new Hrana(odkud,kam,cena);
+                    pole.add(h);
+                }
             }
-
-            Socket sock = new Socket("89.102.55.136", 80);
-
-            DataOutputStream out = new DataOutputStream(sock.getOutputStream());
-            out.writeUTF(builder.toString());
-
-            sock.close();
-
-        } catch (Exception e) {
-            System.out.println("ERROR");
         }
 
-        System.out.println("ERROR");
+        Collections.sort(pole);
+        for (int i = 1; i < pole.size(); i++) {
+            pole.remove(i);
+        }
 
+        for(Hrana h : pole) {
+            System.out.println(h);
+        }
+
+    }
+
+    public static class Hrana implements Comparable<Hrana> {
+
+        int odkud;
+        int kam;
+        int cena;
+
+        @Override
+        public int compareTo(Hrana o) {
+            Integer objektovaCena = this.cena;
+            return objektovaCena.compareTo(o.cena);
+        }
+
+        public Hrana(int odkud, int kam, int cena) {
+            this.odkud = odkud;
+            this.kam = kam;
+            this.cena = cena;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%4d %4d %4d", odkud, kam, cena);
+        }
+        
     }
 
 }
