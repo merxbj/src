c4aa37e0222e6a26cbaa02131a8ccf74646b67e8
merxbj <merxbj@hotmail.com>
2010-10-30 19:20:43 +0200

    Education - Kruskal implementation

--- java/Education/src/pal/Main.java
+++ java/Education/src/pal/Main.java
@@ -1,36 +1,18 @@
-/*
- * Main
- *
- * Copyright (C) 2010  Jaroslav Merxbauer
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package pal;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
 
 /**
  *
- * @author Jaroslav Merxbauer
+ * @author Jiri Fric
  */
 public class Main {
 
@@ -41,9 +23,9 @@ public class Main {
         // TODO code application logic here
         try {
             sit();
-        }
-        catch (Exception ex) {
-            System.out.print("-1");
+        } catch (Exception ex) {
+            //System.out.print(ex);
+            logException(ex);
             System.out.flush();
         }
     }
@@ -52,32 +34,69 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(reader.readLine());//pocet budov
         int I = Integer.parseInt(reader.readLine());//pocet pripojeni
-        List<Hrana> pole = new ArrayList<Hrana>(1000);
-        int odkud =-1;
-        int kam =-1;
-        int cena =-1;
+        List<Hrana> pole = new ArrayList<Hrana>(4000000);
+        int odkud = -1;
+        int kam = -1;
+        int cena = -1;
 
-        while (odkud !=0 || kam!=0 || cena!=0) {
+        while (odkud != 0 || kam != 0 || cena != 0) {
             StringTokenizer st = new StringTokenizer(reader.readLine());
             odkud = Integer.parseInt(st.nextToken());
             kam = Integer.parseInt(st.nextToken());
             cena = Integer.parseInt(st.nextToken());
-            if (odkud!=0 || kam!=0 || cena!=0) {
-                if (odkud > kam){
-                    Hrana h = new Hrana(kam,odkud,cena);
+            if (odkud != 0 || kam != 0 || cena != 0) {
+                if (odkud > kam) {
+                    Hrana h = new Hrana(kam, odkud, cena);
                     pole.add(h);
                 } else {
-                    Hrana h = new Hrana(odkud,kam,cena);
+                    Hrana h = new Hrana(odkud, kam, cena);
                     pole.add(h);
                 }
             }
         }
 
         Collections.sort(pole);
-        for(Hrana h : pole) {
+        for (Hrana h : pole) {
             System.out.println(h);
         }
+        //kruskal
+        //Nastavim do poleVrcholu vsem vrcholum koren na -1, vytvorim tedy N koster, ty pak budu spojovat
+        List<Vrchol> poleVrcholu = new ArrayList<Vrchol>(N);
+        for (int j = 0; j < N; j++) {
+            poleVrcholu.add(new Vrchol(j, -1));
+        }
 
+        int pocStruktur = N;
+        int i = 0;
+
+        int cenaCelkem = 0;
+        while (pocStruktur != I) {
+            int koren1 = findKoren(poleVrcholu, pole.get(i).odkud);
+            int koren2 = findKoren(poleVrcholu, pole.get(i).kam);
+            System.out.println(koren1);
+            System.out.println(koren2);
+            if (koren1 != koren2) {
+                if (koren1 < koren2) {
+                    poleVrcholu.get(koren2).koren = poleVrcholu.get(koren1).koren;
+                } else {
+                    poleVrcholu.get(koren1).koren = poleVrcholu.get(koren2).koren;
+                }
+                cenaCelkem = cenaCelkem + pole.get(i).cena;
+                //System.out.println(pole.get(i).cena);
+                pocStruktur--;
+            }
+            i++;
+        }
+        // System.out.print(cenaCelkem);
+
+    }
+
+    public static int findKoren(List<Vrchol> poleVrcholu, int pozice) {
+        Vrchol bod = poleVrcholu.get(pozice);
+        while (bod.koren != -1) {
+            bod.cislo = bod.koren;
+        }
+        return bod.cislo;
     }
 
     public static class Hrana implements Comparable<Hrana> {
@@ -88,16 +107,16 @@ public class Main {
 
         @Override
         public int compareTo(Hrana o) {
-            int compare = cena.compareTo(o.cena);
+            int compare = odkud.compareTo(o.odkud);
             if (compare != 0) {
                 return compare;
             }
 
-            compare = odkud.compareTo(o.odkud);
+            compare = cena.compareTo(o.cena);
             if (compare != 0) {
                 return compare;
             }
-            
+
             return kam.compareTo(o.kam);
         }
 
@@ -141,7 +160,46 @@ public class Main {
         public String toString() {
             return String.format("%4d %4d %4d", odkud, kam, cena);
         }
-        
     }
 
+    public static class Vrchol {
+
+        Integer cislo;
+        Integer koren;
+
+        public Vrchol(int cislo, int koren) {
+            this.cislo = cislo;
+            this.koren = koren;
+        }
+    }
+
+    static private String formatException(Throwable ex) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ex.toString());
+        sb.append("\n");
+        for (StackTraceElement ste : ex.getStackTrace()) {
+            sb.append("\t at ");
+            sb.append(ste.toString());
+            sb.append("\n");
+        }
+
+        Throwable innerException = ex.getCause();
+        while (innerException != null) {
+            sb.append("\t caused by ");
+            sb.append(innerException.toString());
+            sb.append("\n");
+            innerException = innerException.getCause();
+        }
+
+        return sb.toString();
+    }
+
+    public static void logException(Throwable exception) {
+        log(formatException(exception));
+    }
+
+    private static void log(String message) {
+        Date date = Calendar.getInstance().getTime();
+        System.out.println(String.format("%25s | [%s] | %s", date, "PALY", message));
+    }
 }
