a0a56619d9c0d7160fa87dad5ec2f6412c88387c
merxbj <merxbj@hotmail.com>
2011-11-22 17:07:41 +0100

    Hamurabi - Initial commit - Just a skelet

---
+++ java/Hamurabi/src/hamurabi/application/Application.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hamurabi.application;
+
+import hamurabi.core.HamurabiGame;
+
+/**
+ *
+ * @author jmerxbauer
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        HamurabiGame game = new HamurabiGame();
+        game.play();
+    }
+}
---
+++ java/Hamurabi/src/hamurabi/core/HamurabiGame.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hamurabi.core;
+
+/**
+ *
+ * @author jmerxbauer
+ */
+public class HamurabiGame {
+    
+    public HamurabiGame() {
+        
+    }
+    
+    public void play() {
+        printWelcomeMessage();
+    }
+
+    private void printWelcomeMessage() {
+        System.out.println("TRY YOUR HAND AT GOVERNING ANCIENT SUMERIA");
+        System.out.println("SUCCESSFULLY FOR A 10-YR TERM OF OFFICE.");
+        System.out.println("");
+    }
+    
+}
