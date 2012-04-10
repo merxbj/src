8681503865b88c2a0ea92260ca38831382fd326d
merxbj <merxbj@hotmail.com>
2012-04-10 18:14:37 +0200

    CSharp simple project to test TimeSpan XML representation

---
+++ csharp/SimpleProjects/XmlTimeSpan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace XmlTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan span = XmlConvert.ToTimeSpan("PT24H0M0S");
+            Console.Out.WriteLine(span.TotalSeconds);
+            Console.In.ReadLine();
+        }
+    }
+}
