b72ce4204a2488ae2d934102d9dab1d6e1a1b47c
unknown <jm185267@EURJMERXBAUER2.corp.ncr.com>
2012-10-29 23:12:13 +0100

    GrandMaster - Simple project (initial phase) to parse SC2 GM Page

---
+++ csharp/SimpleProjects/GrandMaster/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace GrandMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest wr = WebRequest.CreateHttp("http://eu.battle.net/sc2/en/ladder/grandmaster");
+            using (Stream stream = wr.GetResponse().GetResponseStream())
+            {
+                using (FileStream fs = new FileStream((args[0]), FileMode.Create))
+                {
+                    stream.CopyTo(fs);
+                }
+            }
+
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(args[1]);
+            xslt.Transform(args[0], args[2]);
+        }
+    }
+}
