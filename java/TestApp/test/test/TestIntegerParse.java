8a41d38082adc9ba6b6db2e541ec92ade5151521
unknown <jm185267@EURJMERXBAUER2.corp.ncr.com>
2013-07-02 12:22:20 +0200

    A couple of updates to various files

---
+++ java/TestApp/test/test/TestIntegerParse.java
@@ -0,0 +1,45 @@
+package test;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jm185267
+ */
+public class TestIntegerParse {
+    
+    public TestIntegerParse() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testIntParse() {
+        assertEquals((int) 010, (int)Integer.decode("010"));
+        assertEquals((int) 8, (int)Integer.decode("010"));
+    }
+}
