8929f85583dcd54b1df315077cc48eae4315fa67
Jaroslav Merxbauer <merxbj@hotmail.com>
2016-05-02 23:30:09 +0200

    PhotoUtils - Backup & Cleanup

---
+++ shell/backup_jarda.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rsync -av --delete ~/Pictures/2016/ /Volumes/My\ Passport/Jarda\ Backup/2016/
+rsync -av --delete ~/Pictures/2015/ /Volumes/My\ Passport/Jarda\ Backup/2015/
+rsync -av --delete /Volumes/DATASTORE/ /Volumes/My\ Passport/DATASTORE\ Backup/
---
+++ shell/backup_lenka.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rsync -av --delete /Users/erralie/Pictures/ /Volumes/My\ Passport/Lenka\ Backup/erralie/Pictures
+rsync -av --delete /Users/erralie/Movies/ /Volumes/My\ Passport/Lenka\ Backup/erralie/Movies/
+rsync -av --delete /Users/erralie/Music/ /Volumes/My\ Passport/Lenka\ Backup/erralie/Music/
+rsync -av --delete /Users/erralie/Desktop/ /Volumes/My\ Passport/Lenka\ Backup/erralie/Desktop/
+rsync -av --delete /Users/erralie/Downloads/ /Volumes/My\ Passport/Lenka\ Backup/erralie/Downloads/
