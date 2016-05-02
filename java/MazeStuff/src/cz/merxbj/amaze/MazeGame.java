890272f43b76ecc86b5fe26d10ca238e78979d31
unknown <jm185267@EURJMERXBAUER2.corp.ncr.com>
2016-05-03 00:33:02 +0200

    Maze - First Maze

--- java/MazeStuff/src/cz/merxbj/amaze/MazeGame.java
+++ java/MazeStuff/src/cz/merxbj/amaze/MazeGame.java
@@ -14,15 +14,41 @@ import java.awt.Graphics;
  * @author merxbj
  */
 public class MazeGame extends Game {
+    public MazeGame() {
+        super();
+        this.width = 1280;
+        this.height = 760;
+        this.padding = 50;
+    }
 
     @Override
     public void init() {
+        maze = new int[][] {
+            {WALL, WALL, WALL, WALL, WALL, PATH, WALL, WALL, WALL, WALL, WALL},
+            {WALL, PATH, PATH, PATH, PATH, PATH, PATH, PATH, WALL, PATH, WALL},
+            {WALL, PATH, WALL, WALL, WALL, WALL, WALL, PATH, WALL, PATH, WALL},
+            {WALL, PATH, WALL, PATH, PATH, PATH, WALL, PATH, PATH, PATH, WALL},
+            {WALL, PATH, WALL, PATH, WALL, PATH, WALL, PATH, WALL, WALL, WALL},
+            {WALL, PATH, WALL, PATH, WALL, PATH, WALL, PATH, PATH, PATH, WALL},
+            {WALL, WALL, WALL, PATH, WALL, PATH, WALL, WALL, WALL, PATH, WALL},
+            {WALL, PATH, PATH, PATH, WALL, PATH, PATH, PATH, PATH, PATH, WALL},
+            {WALL, PATH, WALL, WALL, WALL, WALL, WALL, WALL, WALL, PATH, WALL},
+            {WALL, PATH, PATH, PATH, PATH, PATH, WALL, PATH, PATH, PATH, WALL},
+            {WALL, WALL, WALL, WALL, WALL, PATH, WALL, WALL, WALL, WALL, WALL},
+        };
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.red);
-        g.fillRect(200, 200, 50, 50);
+        int cellSize = (this.height - this.padding*2)/maze.length;
+        g.setColor(Color.black);
+        for (int row = 0; row < maze.length; row++) {
+            for (int col = 0; col < maze[row].length; col++) {
+                if (maze[row][col] == WALL) {
+                    g.fillRect(padding + cellSize*col, padding + cellSize*row, cellSize, cellSize);
+                }
+            }
+        }
     }
 
     @Override
@@ -33,5 +59,9 @@ public class MazeGame extends Game {
     @Override
     public void uninit() {
     }
-    
+
+    private int maze[][];
+    private final int padding;
+    private final int WALL = 0;
+    private final int PATH = 1;
 }
