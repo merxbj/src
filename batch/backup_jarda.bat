677e1485e99356d582a65f33f1a9e856e718741f
Jaroslav Merxbauer <merxbj@hotmail.com>
2018-12-28 12:11:43 +0100

    Backup Scripts Update

--- batch/backup_jarda.bat
+++ batch/backup_jarda.bat
@@ -1,4 +1,5 @@
+robocopy /MIR /v D:\Photography\2018 "F:\Jarda Backup\2018"
 robocopy /MIR /v D:\Photography\2017 "F:\Jarda Backup\2017"
 robocopy /MIR /v D:\Photography\2016 "F:\Jarda Backup\2016"
 robocopy /MIR /v D:\Photography\2015 "F:\Jarda Backup\2015"
-robocopy /MIR /v H:\ "F:\DATASTORE Backup"
+robocopy /MIR /v G:\ "F:\DATASTORE Backup" /XD "G:\SD Card Backups"
--- batch/jpeg_cleanup.bat
+++ batch/jpeg_cleanup.bat
@@ -1,1 +1,1 @@
-java -jar d:\Repos\src\java\Utils\dist\Utils.jar "D:\Photography\2015" "D:\Photography\2016" "D:\Photography\2017" "H:\Jarda Backup\2015" "H:\Jarda Backup\2016" "G:\Production"
+java -jar d:\Repos\src\java\Utils\dist\Utils.jar "D:\Photography\2015" "D:\Photography\2016" "D:\Photography\2017" "D:\Photography\2018" "H:\Jarda Backup\2015" "F:\Jarda Backup\2016" "G:\Production"
