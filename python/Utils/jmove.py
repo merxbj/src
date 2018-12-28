ef4a27a5bb4c4c0c9f5624c7479555cbc63f325f
Jaroslav Merxbauer <merxbj@hotmail.com>
2018-12-28 16:34:34 +0100

    More jpeg scripts

--- python/Utils/jmove.py
+++ python/Utils/jmove.py
@@ -36,11 +36,18 @@ def move_picture(source_path, file_name, capture_date, destination_root):
 
         counter = 0
         while os.path.isfile(destination_path):
-            counter += 1
-            new_file_name = '{}_{:03}'.format(file_name, counter)
-            destination_path = os.path.join(destination_root, destination_subdir, new_file_name)
+            if os.path.getsize(destination_path) == os.path.getsize(source_path):
+                print('WARNING: {} is already in destination. Will delete source.'.format(file_name))
+                destination_path = ''
+            else:
+                counter += 1
+                new_file_name = '{}_{:03}'.format(file_name, counter)
+                destination_path = os.path.join(destination_root, destination_subdir, new_file_name)
 
-        shutil.move(source_path, destination_path)
+        if destination_path != '':
+            shutil.move(source_path, destination_path)
+        else:
+            os.remove(source_path)
 
         print('SUCCESS: {} -> {}'.format(source_path, destination_path))
     except:
@@ -53,8 +60,8 @@ def append_to_backlog(jpg_path, jpg, reason):
 
 
 def main(argv):
-    source = r'h:\Lenka Backup\erralie\Pictures\Fotky Family Library.photoslibrary\Masters'
-    destination = r'h:\Family Fotky'
+    source = r'i:\SD Card Backups'
+    destination = r'f:\Family Fotky'
     print('Source path: {}'.format(source))
     print('Destination path: {}'.format(destination))
 
