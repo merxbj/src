85437b23300e4ba5df9ff9f6b7d140213827799c
jm185267 <Jaroslav.Merxbauer@ncr.com>
2021-05-31 12:43:06 +0200

    Python - Updates to my photo import script

--- python/Utils/photo_import.py
+++ python/Utils/photo_import.py
@@ -58,8 +58,8 @@ def append_to_backlog(jpg_path, jpg, reason):
 
 
 def main(argv):
-    source = r'D:\Photography\2019'
-    destination = r'F:\Family Fotky'
+    source = r'C:\Photography\2020'
+    destination = r'D:\Family Fotky'
     print('Source path: {}'.format(source))
     print('Destination path: {}'.format(destination))
 
