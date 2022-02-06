c744700590acb9591a126dea32a321089455a279
merxbj <merxbj@gmail.com>
2022-02-06 23:35:07 +0100

    Initial implementation of background thread to release the callback ASAP

--- python/power/s0.py
+++ python/power/s0.py
@@ -1,24 +1,19 @@
 import datetime
 import pigpio
-import time
+from threading import Thread
+from asyncio import Event
 
-last_tick = 0
+
+ticks = []
+tick_event = Event()
 
 
 def callback(g, l, t):
-    global last_tick
+    if len(ticks) >= 552000:
+        del ticks[0]
 
-    if last_tick == 0:
-        last_tick = t
-        return
-
-    ticks_since_callback = t - last_tick
-    last_tick = t
-
-    average_watts = (1 * 60 * 60 * 1000 * 1000) / ticks_since_callback
-    time_period = datetime.timedelta(microseconds=ticks_since_callback)
-
-    print("Average watts = " + str(average_watts) + " over " + str(time_period.total_seconds()) + "s")
+    ticks.append((g, l, t, False))
+    tick_event.set()
 
 
 def setup_pulse_monitoring():
@@ -36,10 +31,47 @@ def setup_pulse_monitoring():
     cb1 = pi.callback(pin, pigpio.FALLING_EDGE, callback)
 
 
+def print_tick(tick, previous_tick):
+
+    g, l, t, handled = tick
+    pg, pl, pt, phandled = previous_tick
+
+    if pt == 0:
+        return
+
+    ticks_since_callback = t - pt
+
+    average_watts = (1 * 60 * 60 * 1000 * 1000) / ticks_since_callback
+    time_period = datetime.timedelta(microseconds=ticks_since_callback)
+
+    print("Average watts = " + str(average_watts) + " over " + str(time_period.total_seconds()) + "s")
+
+
+def db_thread():
+    while True:
+        tick_event.wait()
+        tick_event.clear()
+
+        ticks_to_handle = []
+        last_handled_tick = (0, 0, 0, True)
+        for index in range(len(ticks) - 1, -1, -1):
+            if ticks[index][3]:
+                ticks_to_handle.insert(0, ticks[index])
+                ticks[index][3] = True
+            else:
+                last_handled_tick = ticks[index]
+                break
+
+        for tick in ticks_to_handle:
+            print_tick(tick, last_handled_tick)
+            last_handled_tick = tick
+
+
 if __name__ == '__main__':
+
+    db_thread = Thread(target=db_thread)
 
     setup_pulse_monitoring()
     print("Reading impulses ... ")
 
-    while True:
-        time.sleep(1000)
+    db_thread.join()
