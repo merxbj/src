55e99ef10768271268b88a46417d3b793789dc8a
merxbj <merxbj@gmail.com>
2022-02-06 15:25:06 +0100

    Added glitch filter

--- python/lab/s0.py
+++ python/lab/s0.py
@@ -1,4 +1,5 @@
 import datetime
+
 import pigpio, time
 
 last_tick = 0
@@ -10,7 +11,7 @@ def callback(g, l, t):
     last_tick = t
 
     average_watts = (1 * 60 * 60 * 1000 * 1000) / ticks_since_callback
-    time_period = datetime.timedelta(microseconds = ticks_since_callback)
+    time_period = datetime.timedelta(microseconds=ticks_since_callback)
 
     print("\t\tAverage watts = " + str(average_watts) + " over " + str(time_period.total_seconds()) + "s")
 
@@ -25,9 +26,6 @@ pin = 23
 
 pi.set_mode(pin, pigpio.INPUT)
 pi.set_pull_up_down(pin, pigpio.PUD_UP)
+pi.set_glitch_filter(pin, 157000)  # equivalent to 100A across all three phases
 
 cb1 = pi.callback(pin, pigpio.FALLING_EDGE, callback)
-
-while True:
-    print("Direct " + str(pi.read(pin)) + " " + str(pi.get_current_tick()))
-    time.sleep(1000)
