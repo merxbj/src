3ee894cc92307427327747d34cd23397281a5ec8
merxbj <merxbj@gmail.com>
2022-02-06 22:52:29 +0100

    Minor refactoring

--- python/power/s0.py
+++ python/power/s0.py
@@ -7,28 +7,39 @@ last_tick = 0
 
 def callback(g, l, t):
     global last_tick
+
+    if last_tick == 0:
+        last_tick = t
+        return
+
     ticks_since_callback = t - last_tick
     last_tick = t
 
     average_watts = (1 * 60 * 60 * 1000 * 1000) / ticks_since_callback
     time_period = datetime.timedelta(microseconds=ticks_since_callback)
 
-    print("\t\tAverage watts = " + str(average_watts) + " over " + str(time_period.total_seconds()) + "s")
+    print("Average watts = " + str(average_watts) + " over " + str(time_period.total_seconds()) + "s")
 
 
-pi = pigpio.pi()
-if not pi.connected:
-    exit()
+def setup_pulse_monitoring():
+    pi = pigpio.pi()
+    if not pi.connected:
+        exit()
 
-last_tick = pi.get_current_tick()
+    global last_tick
 
-pin = 23
+    pin = 23
 
-pi.set_mode(pin, pigpio.INPUT)
-pi.set_pull_up_down(pin, pigpio.PUD_UP)
+    pi.set_mode(pin, pigpio.INPUT)
+    pi.set_pull_up_down(pin, pigpio.PUD_UP)
 
-cb1 = pi.callback(pin, pigpio.FALLING_EDGE, callback)
+    cb1 = pi.callback(pin, pigpio.FALLING_EDGE, callback)
 
-while True:
+
+if __name__ == '__main__':
+
+    setup_pulse_monitoring()
     print("Reading impulses ... ")
-    time.sleep(1000)
+
+    while True:
+        time.sleep(1000)
