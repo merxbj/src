b69addd00850b8ceedcdbe7061ad363899ae82d3
merxbj <merxbj@gmail.com>
2022-02-10 01:46:58 +0100

    Final implementation of daily totals

--- python/power/web.py
+++ python/power/web.py
@@ -65,9 +65,6 @@ def calc_power(raw_pulses):
 
         last_timestamp = timestamp
 
-    if len(power_readings) == 0:
-        power_readings.append({"timestamp": datetime.utcnow().replace(tzinfo=tz.tzutc()), "power": 0, "duration": 0})
-
     power_readings.reverse()
     return power_readings
 
@@ -169,10 +166,69 @@ def ensure_summary_table_created():
     con.commit()
 
 
-def prepare_daily_data(day_from, day_to):
+def get_total_power_reading(day):
+    raw_total = query_db("""
+                      SELECT * 
+                        FROM total_power_daily
+                       WHERE date(date) = date(:day)
+                """, {"day": day.isoformat()}, one=True)
+    return raw_total
+
+
+def get_pulse_count_for_date(date):
+    filter_from = date
+    filter_from = filter_from.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=tz.tzlocal())
+
+    filter_to = date
+    filter_to = filter_to.replace(hour=23, minute=59, second=59, microsecond=999999, tzinfo=tz.tzlocal())
+
+    params = {"timestamp_from": filter_from.isoformat(), "timestamp_to": filter_to.isoformat()}
+
+    raw_total_pulses = query_db("""          
+          SELECT COUNT(*) AS total_pulses 
+            FROM pulse 
+           WHERE datetime(timestamp) BETWEEN datetime(:timestamp_from) AND datetime(:timestamp_to)
+    """, params, one=True)
+
+    return raw_total_pulses["total_pulses"]
+
+
+def store_total_power(total, date, final):
+    params = {
+        "total_power": total,
+        "day": date.isoformat(),
+        "final": 1 if final else 0
+    }
+    cur = get_db().cursor()
+    cur.execute("INSERT OR REPLACE INTO total_power_daily VALUES(:day, :total_power, :final)", params)
+    cur.close()
+    get_db().commit()
+
+
+def get_daily_total_readings(day_from, day_to):
+
+    daily_total_readings = []
+
     ensure_summary_table_created()
+
     for day in date_range(day_from, day_to + timedelta(days=1)):
-        get_power_readings_for_date(datetime.now())
+        total = 0.0
+        raw_total = get_total_power_reading(day)
+        if raw_total is None or raw_total["final"] == 0:
+            pulse_count = get_pulse_count_for_date(day)
+            if pulse_count > 0:
+                total = pulse_count
+                final = datetime.today().date() > day.date()  # if we are calculating for previous date, it's final
+                store_total_power(total, day, final)
+        else:
+            total = raw_total["total_power"]
+
+        # calculate kWh from total Wh for a day
+        total = total / 1000.0
+
+        daily_total_readings.append({"date": day, "total_power": total})
+
+    return daily_total_readings
 
 
 def render_power_over_month_chart():
@@ -183,7 +239,15 @@ def render_power_over_month_chart():
     day_to = datetime.now()
     day_to = day_to.replace(hour=23, minute=59, second=59, microsecond=999999, tzinfo=tz.tzlocal())
 
-    prepare_daily_data(day_from, day_to)
+    daily_total_readings = get_daily_total_readings(day_from, day_to)
+    df = pd.DataFrame.from_records(daily_total_readings)
+    fig = px.bar(df,
+                 x="date",
+                 y="total_power",
+                 title="Power Totals over last 30 days",
+                 labels={"date": "Date", "total_power": "Power (kWh)"})
+
+    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
 @app.route('/')
@@ -191,10 +255,14 @@ def index():
 
     pod_json = render_power_over_day_chart()
     pb_json = render_power_bar()
-    # pom_json = render_power_over_month_chart()
+    pom_json = render_power_over_month_chart()
     sample_power_readings = get_sample_power_readings()
 
-    return render_template("index.html", samplePowerReadings=sample_power_readings, podJson=pod_json, pbJson=pb_json)
+    return render_template("index.html",
+                           samplePowerReadings=sample_power_readings,
+                           podJson=pod_json,
+                           pbJson=pb_json,
+                           pomJson=pom_json)
 
 
 if __name__ == "__main__":
