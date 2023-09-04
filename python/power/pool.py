d7e745ad122ad0f6bb1e0ab5494faa4c108f24ef
merxbj <merxbj@gmail.com>
2023-09-04 04:17:33 +0200

    Final implementation of pool configuration

--- python/power/pool.py
+++ python/power/pool.py
@@ -18,11 +18,16 @@ import functools
 
 import paho.mqtt.client as mqtt
 
+from threading import Condition
+
 # let's define the Growatt API on a global level for easier access
 growatt_api = None
 
-# let's also the Shelly API on a global level for easier access
+# let's also define the Shelly API on a global level for easier access
 shelly = None
+
+# and finally, define the MQTT Client on a global level for easier access
+mqtt_client = mqtt.Client()
 
 filtration_started_at = None
 
@@ -49,7 +54,12 @@ default_config = """
 }
 """
 
-CONFIG = json.loads(default_config, object_hook=lambda d: SimpleNamespace(**d))
+# the actual configuration used to evaluate pool filtration
+config = json.loads(default_config, object_hook=lambda d: SimpleNamespace(**d))
+
+# an updated configuration that was just received from MQTT and its synchronization object
+updated_config = None
+updated_config_sync = Condition()
 
 class SolarData:
 
@@ -95,17 +105,40 @@ def catch_exceptions(cancel_on_failure=False):
     return catch_exceptions_decorator
 
 
+def on_connect(client, userdata, flags, rc):
+    logging.info("Connected to MQTT with result code {}".format(rc))
+    client.subscribe("power/config")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    try:
+        new_config = json.loads(msg.payload, object_hook=lambda d: SimpleNamespace(**d))
+    except Exception as ex:
+        logging.warning("Failed to parse configuration update message: {}!".format(str(ex)))
+        return
+
+    if hasattr(new_config, "Pool"):
+        new_config = new_config.Pool
+        if new_config != config:
+            global updated_config
+
+            updated_config_sync.acquire()
+            updated_config = new_config
+            updated_config_sync.release()
+
+
 def has_sufficient_power(solar_data):
     # If the battery is almost charged, and we still have power left, let's turn on the filtration
     # Note that 1kW of leftover power will not cover the entire load of pool (0.8kW pump + maybe 3.5kw of heat pump)
     # But better to spend a bit of money to keep the pool clean and warm than give even 1.0kW to grid
-    almost_charged = solar_data.battery_level >= CONFIG.battery_levels.almost_charged and solar_data.leftover_power() > CONFIG.leftover_power.almost_charged
+    almost_charged = solar_data.battery_level >= config.battery_levels.almost_charged and solar_data.leftover_power() > config.leftover_power.almost_charged
 
     # If the battery is clearly charging up rapidly, let's start the filtration a bit sooner
     # The idea is that within the next evaluation cycle, the battery might already be charged and
     # It also takes at least 5 more minutes before the heating kicks in
     # This usually happens during the sunny days
-    rapidly_charging = solar_data.battery_level >= CONFIG.battery_levels.rapidly_charging and solar_data.leftover_power() > CONFIG.leftover_power.rapidly_charging
+    rapidly_charging = solar_data.battery_level >= config.battery_levels.rapidly_charging and solar_data.leftover_power() > config.leftover_power.rapidly_charging
 
     # TODO: Calculate using the battery_charge_power, battery_level and capacity (4*2.56) to come up with better est.
 
@@ -116,14 +149,14 @@ def has_insufficient_power(solar_data, after_hours):
     # Note that based on the battery level we have been clearly discharging for some time
     # Also the 2.5kW difference is likely to turn into a surplus (if we have been heating as well) that will help
     # recharge the battery.
-    rapidly_discharging = solar_data.battery_level < CONFIG.battery_levels.rapidly_discharging and solar_data.leftover_power() < CONFIG.leftover_power.rapidly_discharging
+    rapidly_discharging = solar_data.battery_level < config.battery_levels.rapidly_discharging and solar_data.leftover_power() < config.leftover_power.rapidly_discharging
 
     # Or, we already managed to bring the battery level under 70% - let's switch of filtration and start recharging
-    battery_low = solar_data.battery_level < CONFIG.battery_levels.low
+    battery_low = solar_data.battery_level < config.battery_levels.low
 
     # Or, we are already outside our optional filtration window (after_hours) which is likely in the evening
     # Then, 90% battery is already not enough
-    might_not_recharge = after_hours and solar_data.battery_level < CONFIG.battery_levels.might_not_recharge_afterhours
+    might_not_recharge = after_hours and solar_data.battery_level < config.battery_levels.might_not_recharge_afterhours
 
     # TODO: Calculate using the battery_discharge_power and battery_level to come up with better est.
 
@@ -200,7 +233,7 @@ def evaluate_power_availability():
         except:
             time.sleep(5)
 
-    if now.hour >= CONFIG.scheduler.control_window_closed_from or now.hour <= CONFIG.scheduler.control_window_closed_to:
+    if now.hour >= config.scheduler.control_window_closed_from or now.hour <= config.scheduler.control_window_closed_to:
         if filtration_started_at is None:
             logging.info("Not evaluating available power at this time. Switch is {}.".format(
                 "ON" if switch_on else "OFF"))
@@ -237,7 +270,7 @@ def evaluate_power_availability():
         filtration_runtime = now - filtration_started_at
 
         # Let's also give the filtration some time to run, once we started it, even if it is from grid
-        if filtration_runtime >= timedelta(minutes=CONFIG.scheduler.minimum_runtime_minutes):
+        if filtration_runtime >= timedelta(minutes=config.scheduler.minimum_runtime_minutes):
             if toggle_switch(current_status=switch_on, new_status=False):
                 filtration_started_at = None
 
@@ -303,6 +336,10 @@ if __name__ == '__main__':
                         help="Relay index of the Shelly relay controlling the pump.")
     parser.add_argument("-fsa" "--filtration_started_at", dest="fsa", type=str,
                         help="Specify when the filtration was started at (useful for restarts)")
+    parser.add_argument("-qip" "--mqtt_ip", dest="mqtt_ip", type=str,
+                        help="IP Address of the MQTT broker for Power system configuration.")
+    parser.add_argument("-qp" "--mqtt_port", dest="mqtt_port", type=int,
+                        help="Port of the MQTT broker for Power system configuration.")
 
     args = parser.parse_args()
 
@@ -320,8 +357,14 @@ if __name__ == '__main__':
             logging.error("Failed to initialize Shelly: {}. Will keep trying ...".format(str(ex)))
             time.sleep(5)
 
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(args.mqtt_ip, args.mqtt_port, 60)
+
+    mqtt_client.loop_start()
+
     # Then, schedule a job to check the Solar status every 15 minutes
-    schedule.every(CONFIG.scheduler.period_minutes).minutes.do(evaluate_power_availability)
+    main_job = schedule.every(config.scheduler.period_minutes).minutes.do(evaluate_power_availability)
 
     # Run the job immediately after a startup
     schedule.run_all()
@@ -329,4 +372,26 @@ if __name__ == '__main__':
     # And finally, according to a schedule
     while True:
         schedule.run_pending()
-        time.sleep(1)
+
+        # Did we get a new configuration from MQTT?
+        if updated_config is not None:
+            updated_config_sync.acquire()
+
+            # Log the old and new configuration for convenient comparison
+            logging.info("Old configuration: {}".format(config))
+            logging.info("New configuration: {}".format(updated_config))
+
+            # Update the configuration
+            config = updated_config
+            updated_config = None
+
+            updated_config_sync.release()
+
+            # Reschedule the main job (in case the period has changed)
+            schedule.cancel_job(main_job)
+            main_job = schedule.every(config.scheduler.period_minutes).minutes.do(evaluate_power_availability)
+
+            # And finally, run immediately, just to recheck with the new configuration
+            schedule.run_all()
+
+    mqtt_client.loop_stop(force=False)
