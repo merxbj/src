9c07c551880d70b6214a79dacb1a4b44cea6218f
merxbj <merxbj@gmail.com>
2023-09-20 10:27:07 +0200

    Config improvement and error handling

--- shell/configure_power.sh
+++ shell/configure_power.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mosquitto_pub -h localhost -t power/config -r -f /home/pi/power/config.json
+mosquitto_pub -h localhost -t power/config/pool -r -f /home/pi/config/pool_config.json
+mosquitto_pub -h localhost -t power/config/dehumidifier -r -f /home/pi/config/dehumidifier_config.json
